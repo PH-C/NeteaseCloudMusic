e210e73e15a2af0c5b56e059d44c28a9adc00fac
Non-professionalIFE <cph994@163.com>
2017-10-31 18:25:51 +0800

    2

--- src/containers/Home/Carousel/index.js
+++ src/containers/Home/Carousel/index.js
@@ -3,38 +3,38 @@ import ReactSwipe from 'react-swipe';
 import './index.less'
 
 export default class Carousel extends Component {
-    constructor(){
+    constructor() {
         super()
-        this.state={index:0}
+        this.state = {index: 0}
     }
 
     render() {
         let options = {
             continuous: true,
-            auto:1000,
-            disableScroll:true,
+            auto: 1000,
+            disableScroll: true,
             //每轮播一次会调用此回调方法并传入最新的索引
-            callback:(index)=>{
+            callback: (index) => {
                 this.setState({index});
             }
         };
         return (
             <div className="sliders">
-                {console.log(this.props.sliders)}
+                {/*{console.log(this.props.sliders)}*/}
                 {
-                    this.props.sliders.length>0?<ReactSwipe className="carousel" swipeOptions={options}>
+                    this.props.sliders.length > 0 ? <ReactSwipe className="carousel" swipeOptions={options}>
                         {
-                            this.props.sliders.map((item,index)=>(
-                                <div key={index}><img src={item.pic} style={{width:'100%',height:'160px'}}/></div>
+                            this.props.sliders.map((item, index) => (
+                                <div key={index}><img src={item.pic} style={{width: '100%', height: '160px'}}/></div>
                             ))
                         }
-                    </ReactSwipe>:<div>数据加载中</div>
+                    </ReactSwipe> : <div>数据加载中</div>
                 }
 
                 <div className="dots">
                     {
                         this.props.sliders.map((item, index) => (
-                            <span className={this.state.index == index?"active":""} key={index}></span>
+                            <span className={this.state.index == index ? "active" : ""} key={index}></span>
                         ))
                     }
                 </div>
